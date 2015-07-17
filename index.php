2f6e8be95aa677dabede916409c306f262c003a7
alexandreduparc <duparc.alexandre94@gmail.com>
2015-07-17 11:06:14 +0200

    add main stylesheet

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Github Experience</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+	
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
 	 Modi, obcaecati, qui, incidunt esse numquam commodi enim iusto 
 	 explicabo cumque vero accusamus sint repellendus laudantium nulla neque 
