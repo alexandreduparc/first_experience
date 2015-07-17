c67f2cc3a9842b8c3293e76c3c06f22d9a9d9e97
alexandreduparc <duparc.alexandre94@gmail.com>
2015-07-17 10:52:08 +0200

    Add contents and some headers

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 <head>
 	<meta charset="UTF-8">
 	<title>Github Experience</title>
 </head>
 <body>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+	 Modi, obcaecati, qui, incidunt esse numquam commodi enim iusto 
+	 explicabo cumque vero accusamus sint repellendus laudantium nulla neque 
+	 odio aut laborum accusantium.</p>
 	
 </body>
 </html>
